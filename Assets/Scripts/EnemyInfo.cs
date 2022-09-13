73c9d3693807175322d6e8c0e330760366f26e1d
JangiGabmeister <109713605+JangiGabmeister@users.noreply.github.com>
2022-09-13 12:34:17 +1000

    initial commit

---
+++ Assets/Scripts/EnemyInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : FighterInfo
+{
+    public void Info()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : FighterInfo
+{
+    public void Start()
+    {
+        
+    }
+}
